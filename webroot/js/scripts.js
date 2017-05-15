97587cb7b1d339c0ee4d43482bfa7d7f9822ddf2
Shaun Skeen <shaunpskeen@gmail.com>
2017-05-15 13:31:28 +0100

    colour on attribute values
    script now looks for attribute values and if they're greater

--- webroot/js/scripts.js
+++ webroot/js/scripts.js
@@ -14,6 +14,27 @@ $(document).ready(function() {
     demo();
   })
 
+  $('.label.attribute').each(function(){
+      var attributeValue = $(this).text();
+
+      var className = 'red';
+      if (attributeValue > 60) {
+        className = 'orange';
+      }
+      if (attributeValue > 70) {
+        className = 'yellow'
+      }
+      if (attributeValue > 80) {
+        className = 'green'
+      }
+      if (attributeValue > 90) {
+        className = 'lightgreen'
+      }
+
+      $(this).addClass(className);
+
+  });
+
 });
 
 function demo() {
