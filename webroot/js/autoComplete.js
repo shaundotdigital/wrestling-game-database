94d019dfd69ef38cf93185a97e65ec1c9951a4cb
Shaun Skeen <shaunpskeen@gmail.com>
2017-05-29 19:50:53 +0100

    small style edits

--- webroot/js/autoComplete.js
+++ webroot/js/autoComplete.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   $("input[name=wrestler-search]").autocomplete({
     source: autoCompleteData
   }).data("ui-autocomplete")._renderItem = function(ul, item) {
-      return $("<li class='autocomplete-item'>").data("item.autocomplete", item).append("<a href='/wgdb/wrestlers/view/" + item.id + "' >" + item.label + ', <span class="autocomplete-overall">' + item.overall + '</span>, ' + item.game_name + "</a>").appendTo(ul);
+      return $("<li class='autocomplete-item'>").data("item.autocomplete", item).append("<a href='/wgdb/wrestlers/view/"  + item.id + "' >" + item.overall + ' <span class="autocomplete-wrestler">' + item.label  + '</span><span class="autocomplete-game right">' +  item.game_name + "</span></a>").appendTo(ul);
   };
 
   console.log(autoCompleteData);
