80cc107cb222703a44b2e82b6be41b23820458fe
Shaun Skeen <shaunpskeen@gmail.com>
2017-05-29 15:18:11 +0100

    dropped 'view' from url (now wgdb/wrestlers/1)

--- config/routes.php
+++ config/routes.php
@@ -56,6 +56,9 @@ Router::scope('/', function (RouteBuilder $routes) {
      */
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
+    $routes->connect('/wrestlers/*', ['controller' => 'Wrestlers', 'action' => 'view']);
+
+
     /**
      * Connect catchall routes for all controllers.
      *
