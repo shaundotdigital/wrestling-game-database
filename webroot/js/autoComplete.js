135406e68fb8b08f56df37745e844c439492c07f
Shaun Skeen <shaunpskeen@gmail.com>
2017-05-31 10:27:49 +0100

    added correct data to search

--- webroot/js/autoComplete.js
+++ webroot/js/autoComplete.js
@@ -15,11 +15,16 @@ $(document).ready(function() {
           $('#search-results').empty();
           $.each(data.wrestlers, function(index, wrestler) {
             console.log(wrestler.first_name);
-            $('#search-results').append('<p>'+wrestler.first_name+'<p>');
+            $('#search-results').append('<p>'+wrestler.overall+ ' ' +wrestler.first_name+ ' ' +wrestler.last_name+ ' ' +wrestler.game +'<p>');
           });
 
 
           }
         });
     });
-  });
+
+// append("<a href='/wgdb/wrestlers/view/"  + item.id + "' ><span class='label overall'>" + item.overall + "</span> <span class='autocomplete-wrestler'>" + item.label  + '</span><span class="autocomplete-game right">' +  item.game_name + "</span></a>")
+
+
+
+});
