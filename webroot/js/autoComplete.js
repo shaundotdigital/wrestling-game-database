6851a580c5781be21bb71bea372ae857b7a84c90
Shaun Skeen <shaunpskeen@gmail.com>
2017-06-01 13:29:11 +0100

    search bar style

--- webroot/js/autoComplete.js
+++ webroot/js/autoComplete.js
@@ -15,11 +15,13 @@ $(document).ready(function() {
           console.log(data);
           $('#search-results').empty();
           $.each(data.wrestlers, function(index, wrestler) {
-
-             $('#search-results').append("<p>" +wrestler.wrestler_name+ ' ' +wrestler.game.game_name+ "</p>");
+             $('#search-results').append("<p><span class='label overall'>"+wrestler.overall+"</span><span class='wrestler-name'>"+wrestler.wrestler_name+"</span><span class='game-name'>"+wrestler.game.game_name+ "</span><p>");
             // $('#search-results').append("<p>" +wrestler.first_name+ ' ' +wrestler.last_name+ ' ' +wrestler.game_id+ ' ' +wrestler.overall+ "</p>");
           });
         }
       });
     });
   });
+
+
+// $('#search-results').append("<a href='/wgdb/wrestlers/view/" +wrestler.id+"'><span>"+wrestler.overall+ "</span> <span>" +wrestler.wrestler_name+ '</span><span"> ' +wrestler.game.game_name+ "</span></a>");
