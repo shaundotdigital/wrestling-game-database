85aae54787ce38168559183a6915df5b7f5492a1
Shaun Skeen <shaunpskeen@gmail.com>
2017-06-02 15:46:59 +0100

    search now works 100%

--- webroot/js/autoComplete.js
+++ webroot/js/autoComplete.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
 
+  $(document).click(function(e){
+    var target = e.target;
+    if (!$(target).is('.search-wrapper') ) {
+      console.log('click outside');
+      $('.search-results').empty();
+    }
+  })
+
   $(".search").on('keyup', function(){
     var query = $(this).val();
-    $('.search-results').empty();
 
-    if(query.length == 0) {
+    var $results = $(this).parent().parent().find('.search-results');
+
+    $results.empty();
+
+    if(query.length < 2) {
       return;
     }
 
@@ -12,10 +23,8 @@ $(document).ready(function() {
         url: "/wgdb/wrestlers/search.json",
         data: { query: query },
         success: function(data) {
-          console.log(data);
-          $('.search-results').empty();
           $.each(data.wrestlers, function(index, wrestler) {
-             $('.search-results').append("<li><a href='/wgdb/wrestlers/view/" +wrestler.id+"'><span class='label-search attribute'>"+wrestler.overall+"</span> <span class='wrestler-name'>"+wrestler.wrestler_name+"</span><span class='game-name right'>"+wrestler.game.game_name+ "</span></a></li>");
+             $results.append("<li><a href='/wgdb/wrestlers/view/" +wrestler.id+"'><span class='label-search attribute'>"+wrestler.overall+"</span> <span class='wrestler-name'>"+wrestler.wrestler_name+"</span><span class='game-name right'>"+wrestler.game.game_name+ "</span></a></li>");
           });
         }
       });
